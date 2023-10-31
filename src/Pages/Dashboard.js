83d4904688d3539a1cd5562d18759ad0875d984e
Suresh <shashankyarava1234@gmail.com>
2023-10-31 03:32:10 -0400

    changed dashboard page

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,10 +1,11 @@
 import React from "react";
 import '../Css/dashboard.css';
+import image1 from '../image1.png';
 export default function Dashboard() {
     return (
         <div class="wrapper">
             <div class="sidebar">
-                <h2 style={{marginTop:"-13%"}}>Dashboard</h2>
+                <h2 style={{ marginTop: "-13%" }}>Dashboard</h2>
                 <ul>
                     <li><a href="/trend1">Trend 1</a></li>
                     <li><a href="/trend2">Trend 2</a></li>
@@ -18,22 +19,20 @@ export default function Dashboard() {
                 </div>
             </div>
             <div class="main_content">
-            <header className="trend">
-                <nav>
-                    <ul class="navbar-trend">
-                        <div id="utilities" style={{marginLeft:"70%"}}>
-                        <li><a href="/documentation">Documentation</a></li>
-                        <li><a href="/about">About</a></li>
-                        </div>
-                    </ul>
-                </nav>
-            </header>
-                <div class="header">Hi Dear !!!</div>
-                <div class="info">
-                    <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A sed nobis ut exercitationem atque accusamus sit natus officiis totam blanditiis at eum nemo, nulla et quae eius culpa eveniet voluptatibus repellat illum tenetur, facilis porro. Quae fuga odio perferendis itaque alias sint, beatae non maiores magnam ad, veniam tenetur atque ea exercitationem earum eveniet totam ipsam magni tempora aliquid ullam possimus? Tempora nobis facere porro, praesentium magnam provident accusamus temporibus! Repellendus harum veritatis itaque molestias repudiandae ea corporis maiores non obcaecati libero, unde ipsum consequuntur aut consectetur culpa magni omnis vero odio suscipit vitae dolor quod dignissimos perferendis eos? Consequuntur!</div>
-                    <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A sed nobis ut exercitationem atque accusamus sit natus officiis totam blanditiis at eum nemo, nulla et quae eius culpa eveniet voluptatibus repellat illum tenetur, facilis porro. Quae fuga odio perferendis itaque alias sint, beatae non maiores magnam ad, veniam tenetur atque ea exercitationem earum eveniet totam ipsam magni tempora aliquid ullam possimus? Tempora nobis facere porro, praesentium magnam provident accusamus temporibus! Repellendus harum veritatis itaque molestias repudiandae ea corporis maiores non obcaecati libero, unde ipsum consequuntur aut consectetur culpa magni omnis vero odio suscipit vitae dolor quod dignissimos perferendis eos? Consequuntur!</div>
+                <header className="trend">
+                    <nav>
+                        <ul class="navbar-trend">
+                            <div id="utilities" style={{ marginLeft: "70%" }}>
+                                <li><a href="/documentation">Documentation</a></li>
+                                <li><a href="/about">About</a></li>
+                            </div>
+                        </ul>
+                    </nav>
+                </header>
+                <div class="image-container img">
+                    <img src={image1} alt="Trend slide show" style={{ width: "100%" , marginTop:"10%"}} />
                 </div>
-                <footer id="footer" style={{marginLeft:"20%"}}><p>Disclaimer</p></footer>
+                <footer id="footer" style={{ marginLeft: "20%" }}><p>Disclaimer</p></footer>
             </div>
         </div>
     )
