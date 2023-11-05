f2ad84f1a2ed7d37c643bc50466a659f38f9e7ff
Suresh <shashankyarava1234@gmail.com>
2023-11-04 23:42:23 -0400

    added a paragraph

--- src/MainContent.js
+++ src/MainContent.js
@@ -7,6 +7,7 @@ export default function MainContent(){
         <h1>Trend Analysis of Data</h1>
             <div class="image-container img">
                 <img src={image1} alt="Trend slide show" style={{width:"100%"}}/>
+                <p>Please add a button to count all the rows in all the tables</p>
             </div>
     </div>
     )
