32b2958a698f61d8ff3c84ca4adbcac77e311b44
Suresh <shashankyarava1234@gmail.com>
2023-11-13 22:38:51 -0500

    Added line graph for Trend 1

--- src/Pages/Plot.js
+++ src/Pages/Plot.js
@@ -1,73 +1,65 @@
-import React, { useEffect, useState, useRef, useCallback } from 'react';
-// import Chart from 'chart.js/auto';
-import { Chart, registerables } from 'chart.js';
-Chart.register(...registerables);
+import React from 'react';
+import { Line } from 'react-chartjs-2';
 
-function Plot() {
-    const [data, setData] = useState(null);
-    // const chartRef = React.createRef();
-    const chartRef = useRef();
-    
-    const createChart = useCallback(() => {
-        if (chartRef.current && data) { // Check if chartRef is valid and data is not null
-          // Check if there's an existing chart, and destroy it
-          if (window.myChart) {
-            window.myChart.destroy();
-          }
-      
-          // Create a new chart
-          window.myChart = new Chart(chartRef.current, {
-            type: 'bar',
-            data: {
-              labels: data.map((dataPoint) => dataPoint.label),
-              datasets: [
-                {
-                  label: 'Sample Bar Graph',
-                  data: data.map((dataPoint) => dataPoint.value),
-                  backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                  borderColor: 'rgba(75, 192, 192, 1)',
-                  borderWidth: 1,
-                },
-              ],
-            },
-            options: {
-              scales: {
-                y: {
-                  beginAtZero: true,
-                },
-              },
-            },
-          });
-        }
-      }, [data]);
-      
-    
-    useEffect(() => {
-        fetch('http://127.0.0.1:5000/bar_data')
-          .then((response) => {
-            if (!response.ok) {
-              throw new Error(`Network response was not ok: ${response.status}`);
-            }
-            return response.json();
-          })
-          .then((result) => {
-            setData(result);
-          })
-          .catch((error) => {
-            console.error('Error fetching data:', error);
-          });
-      }, []);
-    
-    useEffect(() => {
-    createChart();
-  }, [data, createChart]);
+import {
+  Chart as ChartJS,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement
+} from 'chart.js';
 
-    return (
-        <div>
-            <label>Sample Bar Chart</label>
-            <canvas ref={chartRef} />
-        </div>
-    );
-}
+
+ChartJS.register(
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement
+)
+
+const Plot = ({ apdata }) => {
+
+  if (!apdata || !apdata.x || !apdata.y) {
+    return null; 
+  }
+
+  const dataset = {
+    labels: apdata.x,
+    datasets: [{
+      labels: 'Years',
+      data: apdata.y,
+      backgroundColor: "aqua",
+      borderColor: 'black'
+      // pointBorderColor: 'aqua'
+    }
+    ]
+
+  }
+
+  const options = {
+    plugins: {
+      legend: true
+    },
+    scales: {
+      x:{
+        min : 0,
+        max : 10
+      },
+      y: {
+        min: 0,
+        max: 10
+      }
+    }
+  }
+
+  return (
+    <div>
+      <Line
+        data={dataset}
+        options={options}
+      ></Line>
+    </div>
+  );
+};
 
 export default Plot;
--- src/Pages/Trend.js
+++ src/Pages/Trend.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React ,{useState} from "react";
 // import chart from "../chart.png";
 import '../Css/trend.css';
 import Plot from "./Plot";
@@ -6,9 +6,9 @@ import Plot from "./Plot";
 
 export default function Trend() {
     return (
-        <div class="wrapper">
-            <div class="sidebar">
-                <a href="/dashboard"><h2 style={{marginTop:"-13%"}}>Dashboard</h2></a>
+        <div className="wrapper">
+            <div className="sidebar">
+                <a href="/dashboard"><h2 style={{ marginTop: "-13%" }}>Dashboard</h2></a>
                 <ul>
                     <li><a href="/trend1">Trend 1</a></li>
                     <li><a href="/trend2">Trend 2</a></li>
@@ -17,80 +17,91 @@ export default function Trend() {
                     <li><a href="/trend5">Trend 5</a></li>
                     <li><a href="/trend6">Trend 6</a></li>
                 </ul>
-                <div class="social_media">
+                <div className="social_media">
                     <a href="\">Logout</a>
 
                 </div>
             </div>
-            <div class="main_content">
+            <div className="main_content">
                 <MainPage />
-                <footer id="footer" style={{marginLeft:"20%"}}><p>Disclaimer</p></footer>
+                <footer id="footer" style={{ marginLeft: "20%" }}><p>Disclaimer</p></footer>
             </div>
         </div>
     )
 }
 
 function MainPage() {
+    const [showInput, setShowInput] = useState(true);
+    const [showPlot, setShowPlot] = useState(false);
+    const [apiData, setApiData] = useState(null);
+  
+    const toggleDivs = () => {
+      setShowInput(!showInput);
+      setShowPlot(!showPlot);
+    };
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+    //   const dropdownValue = document.getElementById("dropdown").value;
+    //   fetch(`https://api.example.com/data?crimeType=${dropdownValue}`)
+    //   .then((response) => {
+    //     if (!response.ok) {
+    //       throw new Error('Network response was not ok');
+    //     }
+    //     return response.json();
+    //   })
+    //   .then((data) => {
+    //     
+    //     // Store the fetched data in state
+    //     setApiData(data1);
+    //     // Toggle divs to hide input and show the plot
+    //     toggleDivs();
+    //   })
+    //   .catch((error) => {
+    //     console.error('Error fetching data:', error);
+    //   });
+    
+    let data1 ={
+                "x": [1, 2, 3, 4, 5],
+                "y": [7, 3, 9, 2, 8]
+              };
+    setApiData(data1);
+    toggleDivs();
+    };
+  
     return (
-        <div>
-            <header className="trend">
-                <nav>
-                    <ul class="navbar-trend">
-                        <li><h1>Trend</h1></li>
-                    </ul>
-                </nav>
-            </header>
-
-
-            <div class="content">
-                <h1 class="element">Analysis of Trend Data</h1>
-                <div class="content-container  grid-container">
-                    <div class="inside1">
-                        <label for="color">Input parameter</label>
-                        <br></br>
-                        <br></br>
-                        <select id="color">
-                            <option value="blue">Product Type</option>
-                            <option value="red">1</option>
-                            <option value="green">2</option>
-                            <option value="yellow">3</option>
-                        </select>
-                        <br></br>
-                        <br></br>
-
-                        <select id="color">
-                            <option value="blue">Geo Location</option>
-                            <option value="red">1</option>
-                            <option value="green">2</option>
-                            <option value="yellow">3</option>
-                        </select>
-
-                        <br></br>
-                        <br></br>
-                        <select id="color">
-                            <option value="blue">Timeframe</option>
-                            <option value="red">1</option>
-                            <option value="green">2</option>
-                            <option value="yellow">3</option>
-                        </select>
-
-                        <br></br>
-                        <br></br>
-                        <select id="color">
-                            <option value="blue">Frequency-Data Point</option>
-                            <option value="red">1</option>
-                            <option value="green">2</option>
-                            <option value="yellow">3</option>
-                        </select>
-                        <br></br>
-                        <br></br>
-                        <button class="button">Submit</button>
-
-                    </div>
-                    <div class="inside2"><Plot /></div>
-                    {/* <img src={chart} alt="chart" /> */}
-                </div>
+      <div>
+        <header className="trend">
+          <nav>
+            <ul className="navbar-trend">
+              <li><h1>Trend</h1></li>
+            </ul>
+          </nav>
+        </header>
+  
+        <div className="content">
+          <h1 className="element">Analysis of Trend Data</h1>
+          <div className="content-container">
+            <div className="inside1" id="input" style={{ display: showInput ? "block" : "none" }}>
+              <br />
+              <br />
+              <form style={{ backgroundColor: "#F3F5F9", display: "flex" }} onSubmit={handleSubmit}>
+                <label htmlFor="dropdown">Select the crime type:</label>
+                <br />
+                <select id="dropdown" name="dropdown">
+                  <option value="option1">Option 1</option>
+                  <option value="option2">Option 2</option>
+                  <option value="option3">Option 3</option>
+                </select>
+                <br />
+                <input type="submit" value="Submit" style={{ background: "blue", color: "white", borderRadius: "3px" }} />
+              </form>
             </div>
+            <div className="inside2" id="plot" style={{ display: showPlot ? "block" : "none" }}>
+            <Plot apdata={apiData}/>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    );
+  }
